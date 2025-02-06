49b8e1b180d215bf8e65b5e418252ab8e828b4a1
SebastianSanchez00 <136854413+SebastianSanchez00@users.noreply.github.com>
2025-02-05 22:02:56 -0300

    Add files via upload
    coomiit

---
+++ anim.js
@@ -0,0 +1,44 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤️"; // Puedes cambiarlo a otro emoji si quieres
+  document.querySelector(".hearts-container").appendChild(heart);
+
+  // Posición aleatoria en la parte inferior
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.fontSize = Math.random() * 20 + 20 + "px";
+
+  // Elimina el corazón cuando termina la animación
+  setTimeout(() => {
+      heart.remove();
+  }, 4000);
+}
+
+function startTransition() {
+  let count = 0;
+  let interval = setInterval(() => {
+      createHeart();
+      count++;
+      if (count >= 400) { 
+          clearInterval(interval);
+       
+      }
+  }, 100);
+}
+
+startTransition()
+
+
+function descargarImagen() {
+  alert()
+  const link = document.createElement("a");
+  link.href = "img/paper.png"; // URL de la imagen (cámbiala por la tuya)
+  link.download = "paper.png"; // Nombre del archivo a descargar
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
---
+++ main.js
@@ -0,0 +1,42 @@
+onload = () =>{
+    document.body.classList.remove("container");
+
+    let audio = document.getElementById("miAudio");
+            audio.currentTime = 78; 
+            // audio.play();
+};
+
+function cargar(){
+    startTransition();
+    alert("Así es, no tienes mas alternativas q decir q si")
+    setTimeout(function() {
+        window.location.href = "/flower.html"
+     }, 3000);
+}
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.innerHTML = "❤️"; 
+    document.querySelector(".hearts-container").appendChild(heart);
+
+  
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 20 + 20 + "px";
+
+    setTimeout(() => {
+        heart.remove();
+    }, 4000);
+}
+
+function startTransition() {
+    let count = 0;
+    let interval = setInterval(() => {
+        createHeart();
+        count++;
+        if (count >= 50) { 
+            clearInterval(interval);
+          
+        }
+    }, 100);
+}
